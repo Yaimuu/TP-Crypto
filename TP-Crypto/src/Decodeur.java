c78577aa3af4bc6290aec153b7d169ce9e857970
Yaimuu <yanis.ouled@gmail.com>
2019-11-16 11:14:08 -0500

    Decodeur + Resultat

--- TP-Crypto/src/Decodeur.java
+++ TP-Crypto/src/Decodeur.java
@@ -1,26 +1,76 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/*
+ * Auteur : Yanis Ouled Moussa, Justin Garcia
+ * Version : 1.0
+ * Fichier : Decodeur.java - Classe permettant de casser les cryptages du server.
+ */
+
 public class Decodeur 
 {
-	private Map<String, Double> frequences = new HashMap<String, Double>();
+	final String ESPACE = " ";
+	final int SUITE = 0 ; //case qui contiendra la suite de lettres
+	final int FREQUENCE = 1 ; //case qui contiendra la fréquence
 	
-	void Decodeur()
+	private Map<String, Double> frequences = new HashMap<String, Double>();;
+	
+	public Decodeur(String fileName) throws IOException 
 	{
-		
+		File file = new File("./" + fileName);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String st;
+		while ((st = br.readLine()) != null) 
+		{
+			String[] parties = st.split(ESPACE);
+			this.frequences.put(parties[SUITE], Math.log(Double.valueOf(parties[FREQUENCE])) );
+		}
+		br.close();
 	}
 	
 	public double calculScore(String phrase)
 	{
-		return 0;
+		double score = 0;
+		for(Map.Entry<String, Double> entry : this.frequences.entrySet())
+		{
+			String key = entry.getKey();
+		    Double value = entry.getValue();
+		    
+		    if(phrase.contains(key))
+		    {
+		    	score += value;
+		    }
+		}
+		return score;
 	}
 	
 	public ArrayList<Integer> calculLongueurCle(String texte, int max)
 	{
-		return new ArrayList<Integer>();
+		ArrayList<Integer> longueurPossible = new ArrayList<Integer>();
+		
+		for(int i = 0; i < max; i++)
+		{
+			String lettresANDansTexte = "";
+			for(int j = 0; j < texte.length(); j+=i)
+			{
+				lettresANDansTexte += texte.charAt(j);
+			}
+			double score = calculeFrequence(lettresANDansTexte);
+			if(Math.abs(score - 0.06) < 0.007)
+			{
+				longueurPossible.add(i);
+			}
+		}
+		
+		return longueurPossible;
 	}
 	
 	public double calculeFrequence(String s) 
@@ -51,13 +101,27 @@ public class Decodeur
 	
 	public Resultat trouveCleNumerique(ArrayList<String> phrasesEncodees, int max)
 	{
-		Resultat result = new Resultat("");
-		return result;
+		Resultat cesarResultat = new Resultat("Cesar");
+		for(int i = 0; i < max; i++)
+		{
+			Ceeeeaaaasaaaaaarrr cesar = new Ceeeeaaaasaaaaaarrr();
+			double score = 0;
+			for(String phrase : phrasesEncodees)
+			{
+				score += calculScore(phrase);
+			}
+			if(score >= 300)
+			{
+				cesarResultat.ajoute(i, score);
+			}
+		}
+		return cesarResultat;
 	}
 	
 	public ArrayList<Resultat> trouveClePhrase(ArrayList<String> phrasesEncodees, int max)
 	{
-		return new ArrayList<Resultat>();
+		ArrayList<Resultat> results = new ArrayList<Resultat>();
+		return results;
 	}
 	
 	public Resultat trouveDictionnaire(ArrayList<String> phrasesEncodees, int scoreTotal)
--- TP-Crypto/src/Resultat.java
+++ TP-Crypto/src/Resultat.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
 
-public class Resultat {
+/*
+ * Auteur : Yanis Ouled Moussa, Justin Garcia
+ * Version : 1.0
+ * Fichier : Resultat.java - Classe permettant de stocker le résultat des décryptions.
+ */
+
+public class Resultat 
+{
 	private String algo;
-	private ArrayList cles = new ArrayList();
+	private ArrayList<Object> cles = new ArrayList<>();
 	private ArrayList<Double> scores = new ArrayList<Double>();
 	
 	
@@ -10,27 +17,61 @@ public class Resultat {
 		this.algo = algo;
 	}
 	
-	public void ajoute(String cle, double score) {
-		
+	public void ajoute(String cle, double score) 
+	{
+		cles.add(cle);
+		scores.add(score);
+		trierListes();
 	}
 	
-	public void ajoute(int cle, double score) {
-		
+	public void ajoute(int cle, double score) 
+	{
+		cles.add(cle);
+		scores.add(score);
+		trierListes();
 	}
 
-	public String getAlgo() {
+	public String getAlgo() 
+	{
 		return algo;
 	}
 
-	public void setAlgo(String algo) {
+	public void setAlgo(String algo) 
+	{
 		this.algo = algo;
 	}
 
-	public ArrayList getCles() {
+	public ArrayList getCles() 
+	{
 		return cles;
 	}
 
-	public void setCles(ArrayList cles) {
+	public void setCles(ArrayList cles) 
+	{
 		this.cles = cles;
+	}
+	
+	/*
+	 * Fonction permettant de trier par ordre décroissant et en fonction des scores les ArrayList scores et cles
+	 */
+	private void trierListes()
+	{
+		double tmp = 0;
+		Object tmpCle;
+		for(int i = 0; i < scores.size(); i++)
+		{
+			for(int j = 1; j < scores.size(); j++)
+			{
+				if(scores.get(j-1) < scores.get(j))
+				{
+					tmp = scores.get(j-1);
+					scores.set(j-1, scores.get(j));
+					scores.set(j, tmp);
+					tmpCle = cles.get(j-1);
+					cles.set(j-1, scores.get(j));
+					cles.set(j, tmp);
+				}
+			}
+		}
 	}
 }
